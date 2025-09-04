0ec25bb0aaf2c7c703190508b678727ff4713977
Ezgi Coban <ezgicoban713@gmail.com>
2025-09-04 21:22:21 +0300

    First commit

---
+++ variables/index.js
@@ -0,0 +1,24 @@
+let x; //declaration
+x =100;  //assignment
+
+let y=59;  //both
+
+console.log(x);
+
+
+let age = 25;
+let price = 5.5;
+let gpa = 3.4;
+
+let name = "ezgi";
+
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`Your gpa is: ${gpa}`);
+
+let online = true;
+
+console.log(`I am online: ${online}`);
+
+console.log(typeof gpa);  //number
+console.log(typeof name);
